f309345826c4793672165c19de7fc2f39ba2cc0c
Davide Andreolli <dvd2003333@gmail.com>
2023-12-02 18:33:53 +0100

    feat: created config content getter #7

---
+++ app/api/configurations/content/[vehicleId]/[deviceId]/[configurationId]/route.ts
@@ -0,0 +1,41 @@
+import 'reflect-metadata';
+import { getDatabase } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+import { ConfigurationMongoContent, RouteParams } from './types';
+
+export async function GET(_: NextRequest, { params }: { params: RouteParams }): Promise<NextResponse> {
+
+  // TODO: Implement authentication
+
+  const db = await getDatabase()
+  const collection = await db.collection("configurations")
+
+  const result = await collection.findOne({
+    vehicleId: params.vehicleId,
+    deviceId: params.deviceId,
+    configurationId: params.configurationId
+  })
+
+  if (!result) {
+    return new NextResponse(null, { status: 404 })
+  }
+
+  const config = plainToInstance(ConfigurationMongoContent, result)
+  const errors = await validate(config)
+  if (errors.length > 0) {
+    return new NextResponse(null, { status: 500 })
+  }
+
+  return new NextResponse(JSON.stringify(config.content), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "X-VehicleId": config.vehicleId,
+      "X-DeviceId": config.deviceId,
+      "X-ConfigurationId": config.configurationId,
+      "X-ConfigurationVersionHash": config.configurationVersionHash
+    }
+  })
+}
---
+++ app/api/configurations/content/[vehicleId]/[deviceId]/[configurationId]/types.ts
@@ -0,0 +1,26 @@
+import { IsNotEmpty, IsObject, IsString, Length } from "class-validator"
+
+export type RouteParams = {
+  vehicleId: string,
+  deviceId: string,
+  configurationId: string
+}
+
+export class ConfigurationMongoContent {
+    @IsString()
+    vehicleId!: string
+
+    @IsString()
+    deviceId!: string
+
+    @IsString()
+    configurationId!: string
+
+    @IsString()
+    @Length(40)
+    configurationVersionHash!: string
+
+    @IsNotEmpty()
+    @IsObject()
+    content!: object
+}
