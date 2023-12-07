f713d7e19a61e6f924b8fbe7f050d13651b40bf6
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-12-07 20:04:18 +0100

    feat(pages): resolver of config schema

--- app/(pages)/config/page.tsx
+++ app/(pages)/config/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import axios, { AxiosResponse } from "axios";
 
 import { Button } from "@/components/ui/button";
@@ -8,9 +8,12 @@ import { Slider } from "@/components/ui/slider";
 
 import { useQuery } from "@tanstack/react-query";
 
-import { getObjectByRef } from "@/lib/utils";
+import { schemaResolve } from "@/lib/utils";
 
 const TelemetryConfig = () => {
+  const [schema, setSchema] = React.useState<any>({});
+  const [content, setContent] = React.useState<any>({});
+
   const configContent = useQuery({
     queryKey: ["telemetry-config", "content"],
     queryFn: async () => {
@@ -32,29 +35,88 @@ const TelemetryConfig = () => {
     enabled: !!configContent.data,
   });
 
+  useEffect(() => {
+    if (configSchema.data && configSchema.isSuccess) {
+      setSchema(schemaResolve(configSchema.data.data, configSchema.data.data));
+    }
+    if (configContent.data && configContent.isSuccess) {
+      setContent(configContent.data.data);
+    }
+  }, [
+    configSchema.data,
+    configSchema.isSuccess,
+    configContent.data,
+    configContent.isSuccess,
+  ]);
+
   return (
     <div className="w-full flex flex-col items-start gap-4 py-8 text-white">
-      {configSchema.data?.data.$ref && (
-        <div>
-          {Object.entries(
-            getObjectByRef(
-              configSchema.data?.data,
-              configSchema.data?.data.$ref
-            ).properties
-          ).map(([key, value]) => (
-            <div key={key} className="w-full flex items-center gap-4">
-              {(value as any).type === "boolean" ? (
-                <>
-                  <p>{key}</p>
-                  <Checkbox />
-                </>
-              ) : (
-                <p>{key}</p>
-              )}
+      {schema.properties && (
+        <div className="w-full flex flex-col gap-4">
+          {Object.entries(schema.properties).map(([key, value]) => (
+            <div key={key} className="w-full flex items-center">
+              {(() => {
+                switch ((value as any).type) {
+                  case "string":
+                    return (
+                      <div className="w-full py-2">
+                        <p className="capitalize">{key}</p>
+                        <input
+                          type="text"
+                          className="w-full rounded-md bg-gray-800 text-white p-2"
+                          placeholder={content[key] as string}
+                        />
+                      </div>
+                    );
+                  case "integer":
+                    return (
+                      <div className="w-full py-2">
+                        {key} {content[key]}
+                      </div>
+                    );
+                  case "boolean":
+                    return (
+                      <div className="w-full flex gap-4 items-center">
+                        <Checkbox checked={content[key]} />
+                        {key}
+                      </div>
+                    );
+                  case "object":
+                    return (
+                      <div className="w-full py-2">
+                        <h3 className="font-medium text-2xl">
+                          {(value as any).title as string}
+                        </h3>
+                        {Object.entries((value as any).properties).map(
+                          ([subkey, subvalue]) => (
+                            <div key={subkey}>
+                              {(() => (
+                                <div className="flex gap-4">
+                                  <p>{subkey}</p>
+                                  <p>{content[key][subkey]}</p>
+                                </div>
+                              ))()}
+                            </div>
+                          )
+                        )}
+                      </div>
+                    );
+                  case "array":
+                    return (
+                      <div className="w-full py-2">
+                        <h3>{key} (array)</h3>
+                      </div>
+                    );
+                  default:
+                    return <p>{key}</p>;
+                }
+              })()}
             </div>
           ))}
         </div>
       )}
+
+      <p>{JSON.stringify(configContent.data?.data, null, 2)}</p>
     </div>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -5,12 +5,35 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const getObjectByRef = (obj: any, ref: string) => {
-  if (!ref) { // TODO: rimuovere questo controllo e gestire il caso in cui ref sia vuoto
-    return obj;
+export const schemaResolve = (schema: any, root: any) => {
+  if (!schema) {
+    return schema;
   }
-  const parts: string[] = ref.split("/").slice(1);
-  return parts.reduce((acc, part) => {
-    return acc[part];
-  }, obj);
+
+  if ("$ref" in schema) {
+    const refPath = schema["$ref"].split("/");
+    let refObj = root;
+
+    for (let i = 1; i < refPath.length; i++) {
+      refObj = refObj[refPath[i]];
+    }
+
+    for (const prop in refObj.properties) {
+      refObj.properties[prop] = schemaResolve(refObj.properties[prop], root);
+    }
+    return refObj;
+  } else if (schema.type === "string") return schema;
+  else if (schema.type === "integer") return schema;
+  else if (schema.type === "boolean") return schema;
+  else if (schema.type === "array") {
+    schema.items = schemaResolve(schema.items, root);
+    return schema;
+  } else if (schema.type === "object") {
+    for (const prop in schema.properties) {
+      schema.properties[prop] = schemaResolve(schema.properties[prop], root);
+    }
+    return schema;
+  } else {
+    console.log("[SCHEMA] ERRORE: ", schema);
+  }
 };
