ca2ee2b9ba5107b9f2c3dadf7b1871b02bde8849
Davide Andreolli <dvd2003333@gmail.com>
2023-12-08 11:35:10 +0100

    feat: created swagger page

---
+++ lib/swagger.ts
@@ -0,0 +1,25 @@
+import { createSwaggerSpec } from 'next-swagger-doc';
+
+export const getApiDocs = async () => {
+  const spec = createSwaggerSpec({
+    apiFolder: 'app/api', // define api folder under app folder
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'E-Agle App Backend Documentation',
+        version: '1.0',
+      },
+      components: {
+        securitySchemes: {
+          BearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+        },
+      },
+      security: [],
+    },
+  });
+  return spec;
+};
