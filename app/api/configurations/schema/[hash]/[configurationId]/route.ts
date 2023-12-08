f054e737cb2e3fa02e70bad8b48591b5f47f18a4
Davide Andreolli <dvd2003333@gmail.com>
2023-12-08 15:29:17 +0100

    feat: added confs schema get docs

--- app/api/configurations/schema/[hash]/[configurationId]/route.ts
+++ app/api/configurations/schema/[hash]/[configurationId]/route.ts
@@ -5,6 +5,34 @@ import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import { getJWT } from '@/lib/auth';
 
+/**
+ * @swagger
+ * /api/configurations/schema/{hash}/{configurationId}:
+ *   get:
+ *     summary: Retrieve the specific version of a configuration schema
+ *     description: While receiving a configuration by calling the getter, schema must be provided as well. This endpoint will return the schema for the specific configuration. The schema is retrieved from the schema binding, which is stored in the database. The schema binding contains the URL to the schema, which is stored in the schema registry. The schema registry will return the schema for the specific configuration.
+ *     tags: [Configurations]
+ *     parameters:
+ *       - in: path
+ *         name: hash
+ *         schema:
+ *           type: string
+ *         required: true
+ *       - in: path
+ *         name: configurationId
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Ok. Configuration has been saved
+ *       401:
+ *         description: Unauthorized. Probably missing or invalid JWT token
+ *       404:
+ *         description: Not found. Schema cannot be found
+ *       500:
+ *         description: Internal Server Error. Something went wrong on the server side
+ */
 export async function GET(
   req: NextRequest,
   { params }: { params: RouteParams }
