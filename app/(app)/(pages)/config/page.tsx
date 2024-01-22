087b8669773b85e079f813e3f851cb448468eecc
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-01-22 12:33:57 +0100

    feat(config): add skeleton to config load

--- app/(app)/(pages)/config/page.tsx
+++ app/(app)/(pages)/config/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Render from "@/components/Render";
+import ConfigSkeleton from "@/components/ConfigSkeleton";
 
 const TelemetryConfig = () => {
   return (
     <div className="w-full flex flex-col items-start gap-4 pb-20 text-white">
-      <React.Suspense fallback={<p>Loading...</p>}>
+      <React.Suspense fallback={<ConfigSkeleton />}>
         <Render
           vehicleId="fenice-evo"
           deviceId="onboard"
---
+++ components/ConfigSkeleton.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { Checkbox } from "@/components/ui/checkbox";
+
+const ConfigSkeleton = () => {
+  return (
+    <div className="space-y-2 w-full py-4">
+        <div className="flex gap-8 items-center py-2">
+            <Checkbox />
+            <Skeleton className="w-[150px] h-[18px] rounded-lg" />
+        </div>
+        
+        <div className="py-2 flex flex-col gap-2">
+            <Skeleton className="w-[100px] h-[20px] rounded-full" />
+            <Skeleton className="w-full h-[35px] rounded-md" />
+        </div>
+        
+        <div className="flex gap-8 items-center py-2">
+            <Checkbox />
+            <Skeleton className="w-[150px] h-[18px] rounded-lg" />
+        </div>
+        
+        <div className="py-2 flex flex-col gap-2">
+            <Skeleton className="w-[100px] h-[20px] rounded-full" />
+            <Skeleton className="w-full h-[35px] rounded-md" />
+        </div>
+
+        <Skeleton className="w-[200px] h-[22px] rounded-full bg-primary" />
+
+        <div className="py-2 flex flex-col gap-2">
+            <Skeleton className="w-[100px] h-[20px] rounded-full" />
+            <Skeleton className="w-full h-[35px] rounded-md" />
+        </div>
+
+        <div className="py-2 flex flex gap-2 items-center">
+            <Skeleton className="w-[200px] h-[20px] rounded-full" />
+            <Skeleton className="w-full h-[35px] rounded-md" />
+        </div>
+
+        <div className="flex gap-8 items-center py-2">
+            <Checkbox />
+            <Skeleton className="w-[150px] h-[18px] rounded-lg" />
+        </div>
+        
+        <div className="py-2 flex flex-col gap-2">
+            <Skeleton className="w-[100px] h-[20px] rounded-full" />
+            <Skeleton className="w-full h-[35px] rounded-md" />
+        </div>
+        
+        <Skeleton className="w-[200px] h-[22px] rounded-full bg-primary" />
+        
+        <div className="py-2 flex flex gap-2 items-center">
+            <Skeleton className="w-[500px] h-[20px] rounded-full" />
+            <Skeleton className="w-full h-[35px] rounded-md" />
+        </div>
+        <div className="py-2 flex flex-col gap-2">
+            <Skeleton className="w-[100px] h-[20px] rounded-full" />
+            <Skeleton className="w-full h-[35px] rounded-md" />
+        </div>
+
+        <div className="flex gap-8 items-center py-2">
+            <Checkbox />
+            <Skeleton className="w-[150px] h-[18px] rounded-lg" />
+        </div>
+
+
+        <div className="py-2 flex flex-col gap-2">
+            <Skeleton className="w-[100px] h-[20px] rounded-full" />
+            <Skeleton className="w-full h-[35px] rounded-md" />
+        </div>
+
+    </div>
+  );
+};
+
+export default ConfigSkeleton;
