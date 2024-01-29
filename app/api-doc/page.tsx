b6de1398a252c9caf6004970f612852fbd04ffc0
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-01-29 13:05:27 +0100

    fix(bg-color-docs): moved theme provider to keep only light mode in documentation

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -4,7 +4,7 @@ import ReactSwagger from '@/components/swagger/swagger';
 export default async function IndexPage() {
   const spec = await getApiDocs();
   return (
-    <section className="container">
+    <section className="container max-md:px-2">
       <ReactSwagger spec={spec} />
     </section>
   );
